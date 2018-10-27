2c5e739626256ef422e1a2da4d5155e1ade079b0
Urbenson Laurent <ulaurent12@gmail.com>
2018-10-27 02:18:49 +0000

    mario duo hill

--- mario/mario.py
+++ mario/mario.py
@@ -1,25 +1,25 @@
+from cs50 import get_int
+
 #define a function to be executed
 def main ():
-    height = -1;
-    
-    while height < 0  or height > 23:
-        height = input("What is the Height? ")
-        #print "Your height is: ", height
-        #type(height)
-    
-for i in range(height-1, -1, -1):
-        for j in range(i):
-            print(" ", end= "")
+    while True:
+        height = get_int("Height? ")
+        if height > 0:
+            break
+
+    for i in range (height-1,-1,-1):
+        for j in range (i):
+            print (" ", end="")
         for k in range((height-i)*2+2):
             if k == (height-i) or k == (height-i)+1:
                 print(" ", end="")
             else:
                 print("#", end="")
 
-        print("")
+        print(" ")
+
 
 if __name__=="__main__":
     main()
 
-    
-    
+
